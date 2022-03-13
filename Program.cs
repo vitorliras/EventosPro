cf755fca31edb5326ed3ffb7336cffec82977abd
Vitor Lira <vitorlirasti@hotmail.com>
2022-03-13 16:50:28 -0300

    Criando e validando o ambiente

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjetoEventos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
